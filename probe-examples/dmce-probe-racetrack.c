93d6cbd3d9c9bffa9f498f5b9f4bdd98e11e5dfa
Christoph Reichenbach <creichen@gmail.com>
2024-02-22 12:54:26 +0000

    aarch64 experimental support

--- probe-examples/dmce-probe-racetrack.c
+++ probe-examples/dmce-probe-racetrack.c
@@ -7,12 +7,16 @@
 
 /* read the cpu timestamp counter */
 static uint64_t dmce_rdtsc(void) {
-#if !defined(__x86_64__)
-#error Architecture not supported
-#else
+#if defined(__x86_64__)
     unsigned int hi, lo;
     __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi) : : "memory");
     return (((uint64_t) hi) << 32) | ((uint64_t) lo);
+#elif defined(__aarch64__)
+    uint64_t val;
+    __asm__ __volatile__ ("mrs %0, cntvct_e10" : "=r"(val) : :);
+    return val;
+#error
+#else Architecture not supported
 #endif
     }
 
